0eb7789a0ddf925d57f02ce2482076004f87e726
Ganesh <havileganesh404@gmail.com>
2022-05-19 07:56:56 +0530

    Project is uploaded

---
+++ CoreJavaBasics/src/com/capgemini/basic/MaximunOfThreeNumbers.java
@@ -0,0 +1,22 @@
+package com.capgemini.basic;
+
+public class MaximunOfThreeNumbers {
+
+	public static void main(String[] args) {
+		
+		int n1=19;
+		int n2=54;
+		int n3=34;
+		if(n1>n2) {
+			System.out.println("Maximum number is n1");
+		}
+		else if(n2>n3) {
+			System.out.println("Maximum number is n2");
+		}
+		else {
+			System.out.println("Maximum number is n3");
+		}
+
+	}
+
+}
